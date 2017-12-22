3fd5a3463de3442c462081f6f6eb76c6707cfe07
JarninFang <jarnin.fang@servicenow.com>
2017-12-22 00:59:02 -0800

    changes

--- main.go
+++ main.go
@@ -7,40 +7,50 @@ import (
   "encoding/json"
 )
 
+type Name struct {
+  First string
+  Last string 
+}
+
 func determineListenAddress() (string, error) {
   port := os.Getenv("PORT") 
   if port == "" {
-    return "", fmt.Errorf("$PORT not set")
+    return "", fmt.Errorf("$PORT not set") } 
+    return ":" + port, nil
   }
-  return ":" + port, nil
-}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Add("Content-Type", "application/json")
-  w.Header().Set("Access-Control-Allow-Origin", "*")
-  if origin := r.Header.Get("Origin"); origin != "" {
-    w.Header().Set("Access-Control-Allow-Origin", origin)
-    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    w.Header().Set("Access-Control-Allow-Headers",
-    "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-  }
-  m := map[string]string{
-    "email": "jarninfang@gmail.com",
-    "website": "jarninfang.github.io",
-  }
-  fmt.Fprintln(w, "Hello World")
-  _ = json.NewEncoder(w).Encode(m)
-  w.WriteHeader(http.StatusCreated)
-}
+  func handler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "application/json; charset=utf-8")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    if origin := r.Header.Get("Origin"); origin != "" {
+      w.Header().Set("Access-Control-Allow-Origin", origin)
+      w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+      w.Header().Set("Access-Control-Allow-Headers",
+      "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    }
+    m := map[string]string{
+      "email": "jarninfang@gmail.com",
+      "website": "jarninfang.github.io",
+      "github_repo_link" : "https://jarninfang.github.io",
+    }
+    _ = json.NewEncoder(w).Encode(m)
 
-func main() {
-  addr, err := determineListenAddress()
-  if err != nil {
-    log.Fatal(err)
+    name := Name{}
+    name.First = "Jarnin"
+    name.Last = "Fang"
+    nameJson,_ := json.Marshal(name)
+    w.Write(nameJson)
+    w.WriteHeader(http.StatusCreated)
   }
-  http.HandleFunc("/",handler)
-  log.Printf("Listening on %s...\n", addr)
-  if err := http.ListenAndServe(addr, nil); err != nil {
-    panic(err)
+
+  func main() {
+    addr, err := determineListenAddress()
+    if err != nil {
+      log.Fatal(err)
+    }
+    http.HandleFunc("/",handler)
+    log.Printf("Listening on %s...\n", addr)
+    if err := http.ListenAndServe(addr, nil); err != nil {
+      panic(err)
+    }
   }
-}
